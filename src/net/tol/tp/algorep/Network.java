eff54c727679b6ab500d45c54b45d25cfda712ed
Sebastien Tolron@gmail.com <sebastien.tolron@gmail.com>
2013-10-24 08:52:16 +0200

    Fichier NetWork

---
+++ src/net/tol/tp/algorep/Network.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.tol.tp.algorep;
+
+/**
+ * @author seb
+ *
+ */
+public class Network extends Thread {
+
+	 public void run() {
+		  
+		    System.out.println("-- Demarrage du threads Network --- \n  ");
+		    
+		    
+		  }
+	 
+}
