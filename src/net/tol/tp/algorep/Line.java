695c1647d7fc746141da62909993193820af2838
Sebastien Tolron@gmail.com <sebastien.tolron@gmail.com>
2013-10-27 14:52:36 +0100

    All the rest

--- src/net/tol/tp/algorep/Line.java
+++ src/net/tol/tp/algorep/Line.java
@@ -1,15 +1,26 @@
 package net.tol.tp.algorep;
 
+/**
+ * This class represent the structure of a line inside the "view" of the queue
+ * 
+ * @author seb
+ * 
+ */
 public class Line {
-	
-	public Line( char message, int horloge) {
+
+	public Line(char message, int horloge) {
 		super();
-	
+
 		this.message = message;
-		this.horloge = horloge;
+		this.timer = horloge;
 	}
 
 	public char message;
-	public int horloge;
+	public int timer;
+
+	@Override
+	public String toString() {
+		return "Line [message=" + message + ", timer=" + timer + "]";
+	}
 
 }
--- src/net/tol/tp/algorep/Main.java
+++ src/net/tol/tp/algorep/Main.java
@@ -1,5 +1,41 @@
+/**
+ * 
+ * 
+ */
 package net.tol.tp.algorep;
 
+/**
+ * This class runs all thread
+ * 
+ * @author seb
+ * 
+ */
 public class Main {
+
+	public static int siteNb = 0;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Mx mutex = new Mx();
+
+		// Application's thread
+
+		Thread tapp = new Thread(new App(mutex));
+		tapp.start();
+
+		// Mx's Thread
+
+		Thread tsec = new Thread(mutex);
+		tsec.start();
+
+		// Network's Thread
+
+		Thread tnet = new Thread(new Network(mutex));
+		tnet.start();
+
+	}
 
 }
